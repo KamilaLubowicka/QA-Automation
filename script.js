f2c203c77e158eace55a7eff6dd2fee14b6dfb8d
Kamila Lubowicka <122810050+KamilaLubowicka@users.noreply.github.com>
2023-01-30 19:47:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ function myColorChange() {
     const element = document.getElementById("id01");
     element.style.color = "red";
 }
+
+
+ZAJĘCIA 3
+
+function myFunction() {
+  const element = document.getElementById("id01");
+  element.innerHTML = "New Heading";
+}
+​
+function myColorChange() {
+  const element = document.getElementById("id01");
+  element.style.color = "red";
+}
+​
+​
+function mojaFunkcja(imie) {
+​
+    const clients = ["Mango", "Poly", "Ajax"];
+    const clientNameToFind = "Patryk";
+    let message;
+    
+    for (const client of clients) {
+    
+      if (client === imie) {
+        message = "Klient z takim imieniem jest w bazie danych!";
+        break;
+      }
+    
+      message = "Nie znaleźliśmy takiego klienta w bazie danych!";
+    }
+    
+    console.log(message);
+    
+​
+}
